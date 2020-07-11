79f11108cd006591c779fac209dfa391c137cfc9
Aratcha Pinprayoon <62659398+aratchamm@users.noreply.github.com>
2020-07-11 22:57:11 +0700

    Edit

--- contact.js
+++ contact.js
@@ -1,9 +1,3 @@
-// COPY TO CLIPBOARD
-// Attempts to use .execCommand('copy') on a created text field
-// Falls back to a selectable alert if not supported
-// Attempts to display status in Bootstrap tooltip
-// ------------------------------------------------------------------------------
-
 function copyToClipboard(text, el) {
   var copyTest = document.queryCommandSupported('copy');
   var elOriginalText = el.attr('data-original-title');
